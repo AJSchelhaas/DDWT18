f2ac51a5f1d30c5d1d87af9ad5eedd6c90608d2e
AJSchelhaas <a.j.schelhaas@student.rug.nl>
2018-11-16 11:33:27 +0100

    Added connect_db and count_series function to model.php Added series counter to card.php

--- week1/model.php
+++ week1/model.php
@@ -119,3 +119,35 @@ function get_error($feedback){
         </div>';
     return $error_exp;
 }
+
+/**
+ * @param string host
+ * @param string database
+ * @param string username
+ * @param string password
+ * @return PDO
+ */
+function connect_db($host, $db, $user, $pass){
+    $charset = 'utf8mb4';
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ];
+    try {
+        $pdo = new PDO($dsn, $user, $pass, $options);
+    } catch (\PDOException $e) {
+        echo sprintf("Failed to connect. %s",$e->getMessage());
+    }
+    return $pdo;
+}
+
+/**
+ * @param PDO
+ */
+function count_series($pdo) {
+    $stmt = $pdo->prepare('SELECT * FROM series');
+    $stmt->execute();
+    $series_count = $stmt->rowCount();
+    return $series_count;
+}
--- week1/views/cards.php
+++ week1/views/cards.php
@@ -1,11 +1,13 @@
 <!-- Series count -->
+
+
 <div class="card">
     <div class="card-header">
         Series
     </div>
     <div class="card-body">
         <p class="count">Series overview already has</p>
-        <h2>3</h2>
+        <h2><?php echo count_series($pdo) ?></h2>
         <p>series listed</p>
         <a href="/ddwt18/week1/add/" class="btn btn-primary">List yours</a>
     </div>
